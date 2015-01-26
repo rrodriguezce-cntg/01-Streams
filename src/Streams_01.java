1eeaf0980b7c1287a19390263adef99894f04e7f
rrodriguezce-CNTG <tarde@10.0.1.132>
2015-01-26 20:37:27 +0100

    Ejercicio 1

---
+++ src/Streams_01.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.ToDoubleFunction;
+
+
+public class Streams_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		List<Factura> facturas = Arrays.asList(
+				new Factura("Cliente 1", 1),
+				new Factura("Cliente 2", 2),
+				new Factura("Cliente 3", 3),
+				new Factura("Cliente 4", 4),
+				new Factura("Cliente 5", 5)
+				);
+				
+//		facturas.stream().mapToDouble(new ToDoubleFunction<Factura>() {
+//			@Override
+//			public double applyAsDouble(Factura value) {
+//				// TODO Auto-generated method stub
+//				return 0;
+//			}
+//		});
+		
+		facturas.stream().mapToDouble(f -> f.getCantidad());
+
+		facturas.stream().mapToDouble((Factura f) -> f.getCantidad());
+
+	}
+
+}
