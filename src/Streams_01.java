9be4ba9f7164c7c42bb98836a59d9c37e3a9cf3c
rrodriguezce-CNTG <tarde@10.0.1.132>
2015-01-27 17:08:15 +0100

    Update

--- src/Streams_01.java
+++ src/Streams_01.java
@@ -8,7 +8,7 @@ public class Streams_01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		List<Factura> facturas = Arrays.asList(
 				new Factura("Cliente 1", 1),
 				new Factura("Cliente 2", 2),
@@ -16,18 +16,27 @@ public class Streams_01 {
 				new Factura("Cliente 4", 4),
 				new Factura("Cliente 5", 5)
 				);
-				
-//		facturas.stream().mapToDouble(new ToDoubleFunction<Factura>() {
-//			@Override
-//			public double applyAsDouble(Factura value) {
-//				// TODO Auto-generated method stub
-//				return 0;
-//			}
-//		});
-		
-		facturas.stream().mapToDouble(f -> f.getCantidad());
 
+		//		facturas.stream().mapToDouble(new ToDoubleFunction<Factura>() {
+		//			@Override
+		//			public double applyAsDouble(Factura value) {
+		//				// TODO Auto-generated method stub
+		//				return 0;
+		//			}
+		//		});
+
+		//Streams
+		facturas.stream().mapToDouble(f -> f.getCantidad());
 		facturas.stream().mapToDouble((Factura f) -> f.getCantidad());
+		facturas.stream().mapToDouble(f -> f.getCantidad()).filter(d -> d > 2).forEach(s -> System.out.println(s));//System.out::println	
+
+		
+		System.out.println("\n");
+		
+		
+		//Streams en paralelo
+		facturas.parallelStream().mapToDouble(f -> f.getCantidad()).filter(d -> d > 2).forEach(s -> System.out.println(s));
+
 
 	}
 
